124db9c3698d5eab6330dc7efaea53f11566e5b3
Marjac <m.jacobs@student.tue.nl>
2017-06-21 21:59:08 +0200

    Added the "till" :')

--- js/index.js
+++ js/index.js
@@ -14,8 +14,10 @@ function lower() {
 }
 
 function timeUpdate() {
-    $("#day").text(get("day", "current_day"));
-    $("#clock").text(get("time", "time"));
+    Time = get("time", "time")
+    CurrentDay = get("day", "current_day")
+    $("#day").text(CurrentDay);
+    $("#clock").text(Time);
 }
 
 function currentTemp() {
@@ -37,7 +39,7 @@ function currentTemp() {
         $("#up").hide();
         $("#dup").hide();
     }
-
+    $("#tillTemp").text(nextProgramTime());
 }
 
 function setTarget(temp) {
@@ -50,8 +52,49 @@ function getTarget() {
     setTarget(parseFloat(get("targetTemperature", "target_temperature")));
 }
 
+function timesDay(day){
+    var times = getProgram(day)
+    times = _.flatten(times);
+    times.sort(function(a, b) {
+        return parseTime(a) - parseTime(b);
+    });
+    return times;
+}
+
+function timeComparision(time){
+    return parseTime(time) >= parseTime(Time);
+}
+
+function nextProgramTime() {
+    return timesDay(CurrentDay).find(timeComparision) || getProgram(nextDay(CurrentDay))[0][0];
+}
+
+function nextDay(day){
+    if(day===Days.Monday){
+        return Days.Tuesday
+    }
+    if(day===Days.Tuesday){
+        return Days.Wednesday
+    }
+    if(day===Days.Wednesday){
+        return Days.Thursday
+    }
+    if(day===Days.Thursday){
+        return Days.Friday
+    }
+    if(day===Days.Friday){
+        return Days.Saturday
+    }
+    if(day===Days.Saturday){
+        return Days.Sunday
+    }
+    if(day===Days.Sunday){
+        return Days.Monday
+    }
+}
+
 function init() {
-    put("time", "time", "20:00");
+    put("time", "time", "11:00");
     put("day", "current_day", Days.Monday);
     timeUpdate();
 
@@ -83,5 +126,21 @@ function init() {
     window.setInterval(currentTemp, 1000);
 
     window.setInterval(getTarget, 1000);
+    
+    setDefault();
+    addPeriod(Days.Monday, "12:00", "14:00");
+    addPeriod(Days.Monday, "17:00", "21:00");
+    addPeriod(Days.Tuesday, "12:00", "14:00");
+    addPeriod(Days.Tuesday, "22:00", "23:00");
+    addPeriod(Days.Wednesday, "12:00", "14:00");
+    addPeriod(Days.Wednesday, "15:30", "19:00");
+    addPeriod(Days.Thursday, "12:00", "14:00");
+    addPeriod(Days.Friday, "16:30", "22:00");
+    addPeriod(Days.Saturday, "12:00", "14:00");
+    addPeriod(Days.Sunday, "17:00", "23:30");
+    getWeekProgram();
+
+    put("weekProgramState", "week_program_state", "on");
+
 
 }
