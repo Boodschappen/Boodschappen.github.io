88b4881c7c2d9674685c2fee075772a12d094424
Boodschappen <ameyarao98@gmail.com>
2017-06-23 19:13:28 +0200

    Create ts.js

---
+++ js/ts.js
@@ -0,0 +1,58 @@
+
+window.addEventListener("load", init, false);
+
+function timeSet(){
+    put("time", "time", "11:00");
+    put("day", "current_day", Days.Monday);
+}
+
+function init() {
+
+    timeUpdate();
+
+
+    //slider
+    mySlider = $("#slid").slider({
+        reversed: true,
+        step: step,
+    });
+
+    getTarget();
+
+   function dayTemp(temp) {
+    mySlider.slider('setValue', temp);
+    put("dayTemperature", "day_temperature");
+}
+
+    $("#dayMode").on("click", function () {
+        setTarget(parseFloat(get("dayTemperature", "day_temperature")));
+    });
+
+    $("#nightMode").on("click", function () {
+        setTarget(parseFloat(get("nightTemperature", "night_temperature")));
+    });
+
+    //runtime
+    window.setInterval(timeUpdate, 1000);
+    //gettemp
+    window.setInterval(currentTemp, 1000);
+
+    window.setInterval(getTarget, 1000);
+    
+    setDefault();
+    addPeriod(Days.Monday, "12:00", "14:00");
+    addPeriod(Days.Monday, "17:00", "21:00");
+    addPeriod(Days.Tuesday, "12:00", "14:00");
+    addPeriod(Days.Tuesday, "22:00", "23:00");
+    addPeriod(Days.Wednesday, "12:00", "14:00");
+    addPeriod(Days.Wednesday, "15:30", "19:00");
+    addPeriod(Days.Thursday, "12:00", "14:00");
+    addPeriod(Days.Friday, "16:30", "22:00");
+    addPeriod(Days.Saturday, "12:00", "14:00");
+    addPeriod(Days.Sunday, "17:00", "23:30");
+    getWeekProgram();
+    
+    weekProgramOn();
+
+
+}
