e5905145a18c46ffc838e849847093bee894b93a
Marjac <m.jacobs@student.tue.nl>
2017-06-24 11:53:38 +0200

    Only the remove a period funtion must be implemented

--- js/mondayTime.js
+++ js/mondayTime.js
@@ -2,6 +2,18 @@ window.addEventListener("load", init, false);
 
 var mycount = 0;
 
+$("#saveProgram").click(function () {
+    var num = getProgram(Days.Monday).length;
+    for (var i = 1; i <= num; i++) {
+        removePeriod(Days.Monday, 0);
+    }
+
+    for (var i = 1; i <= mycount; i++) {
+        addPeriod(Days.Monday, $("#day" + i).val(), $("#night" + i).val());
+    }
+
+});
+
 $("#add").click(function (ev) {
     mycount = mycount + 1;
 
@@ -22,9 +34,26 @@ $("#add").click(function (ev) {
     }
 });
 
+function fillWeek() {
+    getWeekProgram();
+    var times = getProgram(Days.Monday)
+
+    for (var i = 0; i < times.length; i++) {
+        $('#day' + (i + 1)).timepicker('setTime', times[i][0]);
+        $('#night' + (i + 1)).timepicker('setTime', times[i][1]);
+        $('#row' + (i + 1)).show();
+        mycount++;
+    }
+}
+
+
+
+
+
 function init() {
     mycount = 0;
     for (var i = 1; i <= 5; i++) {
         $("#row" + i).hide();
     }
+    fillWeek();
 }
